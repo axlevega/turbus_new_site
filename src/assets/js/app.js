f15d48ae976b238980562644ebd7f6daf3a81f0b
axlevega <axle0vega@yandex.ru>
2024-08-13 00:08:49 +0700

    feat: Делаю карусель направлений

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,8 @@
 import $ from 'jquery'; // Jquery
 import './utils.js'; // Дополнительная логика (Модалки, якорные ссылки)
 
+import './swiper_items.js';
+
 window.jQuery = $;
 window.$ = $;
+
---
+++ src/assets/js/swiper_items.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+// Карусель из направлений туров
+const swiper_directs_carousel = new Swiper('.directs_carousel .swiper', {
+    slidesPerView: 3.7,
+    modules: [Scrollbar],
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
