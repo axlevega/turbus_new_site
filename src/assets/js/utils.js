68d82e4e7981d23ba7c833e29272c685dc4f8eb7
axlevega <axle0vega@yandex.ru>
2024-08-18 15:36:57 +0700

    feat: Сделал блок с ближайшими направлениями

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -66,6 +66,55 @@ $(".go_to_top").on("click", function () {
   });
 });
 
+// Auto height
+function unique(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+$(document).ready(function () {
+  let heightsArray = [];
+  $("[similar-height]").each(function () {
+    heightsArray.push($(this).attr("similar-height"));
+  });
+
+  heightsArray = unique(heightsArray);
+
+  for (let heightName in heightsArray) {
+    let maxHeight = 0;
+    $("[similar-height=" + heightsArray[heightName] + "]").each(function () {
+      if ($(this).height() > maxHeight) {
+        maxHeight = $(this).height();
+      }
+    });
+    $("[similar-height=" + heightsArray[heightName] + "]").each(function () {
+      $(this).height(maxHeight);
+    });
+  }
+
+  $(window).on("resize", function () {
+    for (let heightName in heightsArray) {
+      let maxHeight = 0;
+      $("[similar-height=" + heightsArray[heightName] + "]").each(function () {
+        $(this).height("auto");
+        if ($(this).height() > maxHeight) {
+          maxHeight = $(this).height();
+        }
+      });
+      $("[similar-height=" + heightsArray[heightName] + "]").each(function () {
+        $(this).height(maxHeight);
+      });
+    }
+  });
+});
+
 ////////////////////////////////////
 /////// Табы ///////////////////////
 
